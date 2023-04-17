7d51d17d2169c29aa0da3a3bb57f9d3bb6495e71
Jaehaerys <130801761+DevJaehaerys@users.noreply.github.com>
2023-04-17 22:59:37 +0300

    Update Modal.jsx

--- resources/js/Components/Modal.jsx
+++ resources/js/Components/Modal.jsx
@@ -78,7 +78,7 @@ function Modal({ selectedItem, handleModalClose, auth }) {
                         </label>
                     </div>
                     {error && (
-                        <div className="alert alert-error shadow-lg">
+                        <div className="alert alert-error mt-5 shadow-lg">
                             <div>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                                 <span>{error}</span>
