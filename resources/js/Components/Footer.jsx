f852798febd96baf3e9c0454391ba4f84de86e6e
Jaehaerys <130801761+DevJaehaerys@users.noreply.github.com>
2023-04-18 10:09:14 +0300

    Update Footer.jsx
    Show server online

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -1,8 +1,10 @@
+import Online from "@/Components/elements/Online";
 
 const Footer = () => {
     return (
         <footer className="footer footer-center p-4 bg-base-300 text-base-content">
             <div>
+                <Online/>
                 <p>Copyright © 2023 - JCMS Lite. </p>
             </div>
         </footer>
