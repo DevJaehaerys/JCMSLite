8daafdf4936cd01d5600fa1833d11d503299022f
Jaehaerys <130801761+DevJaehaerys@users.noreply.github.com>
2023-04-18 09:11:46 +0300

    Update Blank.jsx

--- resources/js/Pages/Blank.jsx
+++ resources/js/Pages/Blank.jsx
@@ -1,7 +1,7 @@
 import Page from "@/Layouts/Page";
 function Blank({auth}) {
     return (
-        <Page auth={auth} name="Home">
+        <Page auth={auth} name="Blank">
             <div className="hero">
                 <div>
                     <h1>Blank page</h1>
