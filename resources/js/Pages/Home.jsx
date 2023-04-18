a67890ed9e3fa468eaee6f8e825681e258ecdc8a
Jaehaerys <130801761+DevJaehaerys@users.noreply.github.com>
2023-04-18 09:11:34 +0300

    Update Home.jsx

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -2,7 +2,7 @@ import Page from "@/Layouts/Page";
 import Shopcard from "@/Components/elements/shopcard";
 function Home({auth, shop}) {
     return (
-        <Page auth={auth} name="Blank">
+        <Page auth={auth} name="Home">
             <div className="hero">
                 <div>
                     <Shopcard auth={auth} shop={shop}/>
