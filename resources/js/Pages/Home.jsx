18bec5152660f1277f04932c9b9696b1e813610f
Jaehaerys <130801761+DevJaehaerys@users.noreply.github.com>
2023-04-18 09:10:57 +0300

    fix props
    fix props

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -2,7 +2,7 @@ import Page from "@/Layouts/Page";
 import Shopcard from "@/Components/elements/shopcard";
 function Home({auth, shop}) {
     return (
-        <Page auth={auth} name="Home">
+        <Page auth={auth} name="Blank">
             <div className="hero">
                 <div>
                     <Shopcard auth={auth} shop={shop}/>
