bb01d5663eeb540fc61658d62b8aeca2efecdcc0
Jaehaerys <130801761+DevJaehaerys@users.noreply.github.com>
2023-04-19 15:42:49 +0300

    add paypal and payments config

--- resources/js/Data/Payments.jsx
+++ resources/js/Data/Payments.jsx
@@ -1,22 +1,40 @@
-import React from 'react';
+import { router } from '@inertiajs/react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Payment = ({ methods }) => {
+function Payments({ paymentMethods, buttonLabel, balance }) {
+    const handleClick = async (paymentMethod) => {
+        try {
+            const response = await router.post(`/payment/${paymentMethod}/redirect`, { Balance: balance });
+        } catch (error) {
+            toast.error(`🦄 `, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+        }
+    };
+
     return (
-        <div className="payment-methods">
-            {methods.map(method => (
-                <div key={method.id} className="payment-method">
-                    <img src={method.icon} alt={method.name} />
-                    <h3>{method.name}</h3>
-                    <p>Minimum amount: {method.minAmount}</p>
-                    {method.enabled ? (
-                        <p>Status: enabled</p>
-                    ) : (
-                        <p>Status: disabled</p>
-                    )}
-                </div>
-            ))}
+        <div>
+            {paymentMethods.qiwi && (
+                <button className="btn mr-2" onClick={() => handleClick('qiwi')}>
+                    {buttonLabel} with QIWI
+                </button>
+            )}
+            {paymentMethods.paypal && (
+                <button className="btn mr-2" onClick={() => handleClick('paypal')}>
+                    {buttonLabel} with PayPal
+                </button>
+            )}
+            <a href="#" className="btn">Close</a>
         </div>
     );
-};
+}
 
-export default Payment;
+export default Payments;
