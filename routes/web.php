998a16a769b99f814353ab5d77e0a5d8fb01bd51
Jaehaerys <130801761+DevJaehaerys@users.noreply.github.com>
2023-04-19 15:04:00 +0300

    add paypal

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\Payment\QiwiController;
+use App\Http\Controllers\Payment\PayPalController;
 use Inertia\Inertia;
 
 /*
@@ -25,7 +26,20 @@ route::get('/blank', [PagesController::class, 'blank'])->name('blank');
 route::post('/getItemInfo', [ShopController::class, 'getItem'])->name('getItem');
 route::post('/buyItem', [ShopController::class, 'buyItem'])->name('buyItem');
 
-Route::post('/payment/qiwi/handler', [QiwiController::class, 'qiwiHandler']);
-Route::post('/payment/qiwi/redirect', [QiwiController::class, 'qiwiRedirect']);
+Route::controller(QiwiController::class)
+    ->prefix('payment/qiwi/')
+    ->group(function () {
+        Route::post('handler', 'qiwiHandler')->name('qiwi.handler');
+        Route::post('redirect', 'qiwiRedirect')->name('qiwi.redirect');
+    });
+
+Route::controller(PayPalController::class)
+    ->prefix('payment/paypal/')
+    ->group(function () {
+        Route::post('redirect', 'handlePayment')->name('make.payment');
+        Route::get('cancel-payment', 'paymentCancel')->name('cancel.payment');
+        Route::get('payment-success', 'paymentSuccess')->name('success.payment');
+    });
+
 
 require __DIR__.'/auth.php';
