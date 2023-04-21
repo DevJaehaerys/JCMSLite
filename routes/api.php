a77b053dd3a0464e386a54e75394ca87b7c21114
Jaehaerys <130801761+DevJaehaerys@users.noreply.github.com>
2023-04-21 21:29:06 +0300

    api update

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CartController;
+use App\Http\Controllers\Api\ServerController;
+use App\Http\Controllers\Api\ShopController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,9 +14,14 @@ use App\Http\Controllers\Api\CartController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
 Route::group(['middleware' => 'apisecurity'], function () {
-    Route::get('/items', [CartController::class, 'allItems']);
-    Route::get('/userItem/{id}', [CartController::class, 'userItems']);
-    Route::get('/removeItem/{id}', [CartController::class, 'removeItem']);
+
+    Route::get('/cart/items', [CartController::class, 'items']); // get all items
+    Route::get('/cart/userItem/{id}', [CartController::class, 'add']); // get user cart by steamid
+    Route::get('/cart/removeItem/{id}', [CartController::class, 'remove']); // remove item from cart by ID
+    Route::get('/shop/items', [ShopController::class, 'items']); // get all items
+    Route::post('/shop/addItem/', [ShopController::class, 'add']); // add item to SHOP
+    Route::get('/shop/removeItem/{id}', [ShopController::class, 'remove']); // remove item from cart by ID
 });
+
+Route::get('/online', [ServerController::class, 'serverData']); // get server online
