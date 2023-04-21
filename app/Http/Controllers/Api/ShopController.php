326bd0a50aebe72db683fb4a22d5e1d27f969ff0
Jaehaerys <130801761+DevJaehaerys@users.noreply.github.com>
2023-04-21 21:30:54 +0300

    update api

---
+++ app/Http/Controllers/Api/ShopController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Shop;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ShopController extends Controller
+{
+    public function items()
+    {
+        $items = Shop::with('user')->get();
+        return response()->json($items);
+    }
+    public function add(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'image' => 'required',
+            'about' => 'required',
+            'price' => 'required|numeric',
+            'command' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $record = new Shop();
+        $record->name = $request->input('name');
+        $record->image = $request->input('image');
+        $record->about = $request->input('about');
+        $record->price = $request->input('price');
+        $record->command = $request->input('command');
+        $record->save();
+
+        return response()->json(['success' => true]);
+    }
+    public function remove($itemId)
+    {
+        $item = Shop::findOrFail($itemId);
+        $item->delete();
+        return response()->json(['success' => true]);
+    }
+}
