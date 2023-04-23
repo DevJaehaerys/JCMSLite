55df9283f13d26ed24f4ea1ef4deb73c40ff8df5
Jaehaerys <130801761+DevJaehaerys@users.noreply.github.com>
2023-04-23 09:05:44 +0300

    new payment methods

--- config/payments.php
+++ config/payments.php
@@ -1,7 +1,18 @@
 <?php
 return [
-    'qiwi_public_key' => 'ваш паблик ключ',
-    'qiwi_secret_key' => 'ваш секретный ключ',
-    'qiwi_success_url' => 'https://cms.jaehaerys.dev/',
-    'qiwi_commission' => 10,
+    'qiwi_public_key' => env('QIWI_PUBLIC_KEY', ''),
+    'qiwi_secret_key' => env('QIWI_SECRET_KEY', ''),
+    'qiwi_success_url' => env('QIWI_SUCCESS_URL', ''),
+    'qiwi_commission' => env('QIWI_COMISSION', ''),
+
+    'centapp_shop_id' => env('CENTAPP_SHOP_ID', ''),
+    'centapp_token' => env('CENTAPP_TOKEN', ''),
+
+    'fk_project_id' => env('FK_PROJECT_ID', ''),
+    'fk_secret_word' => env('FK_SECRET_WORD', ''),
+    'fk_currency' => env('FK_CURRENCY', 'RUB'),
+
+    'enot_merchant_id' => env('ENOT_PROJECT_ID', ''),
+    'enot_secret_word' => env('ENOT_SECRET_WORD', ''),
+    'enot_secret_word2' => env('ENOT_SECRET_WORD2', ''),
 ];
